d4930d11f9f5f1f469263e508b56c55f9f7281d6
kvukovic4 <katarina.vukovic3@gmail.com>
2022-02-15 20:12:13 +0100

    init

---
+++ dom-kantrida/firebase.conf.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBG7H711L0O5DPVz-TqRCGC-_b96W2kWec",
+  authDomain: "dom-kantrida.firebaseapp.com",
+  projectId: "dom-kantrida",
+  storageBucket: "dom-kantrida.appspot.com",
+  messagingSenderId: "89658383943",
+  appId: "1:89658383943:web:0d24c894e0680c8e2b9b62",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

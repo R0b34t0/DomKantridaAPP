b616f3ad081e181b20a2c57b0bb9a163a17ff5e6
kvukovic4 <katarina.vukovic3@gmail.com>
2022-02-15 20:41:40 +0100

    dotenv, prettier settings

--- dom-kantrida/firebase.conf.js
+++ dom-kantrida/firebase.conf.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBG7H711L0O5DPVz-TqRCGC-_b96W2kWec",
-  authDomain: "dom-kantrida.firebaseapp.com",
-  projectId: "dom-kantrida",
-  storageBucket: "dom-kantrida.appspot.com",
-  messagingSenderId: "89658383943",
-  appId: "1:89658383943:web:0d24c894e0680c8e2b9b62",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 // Initialize Firebase

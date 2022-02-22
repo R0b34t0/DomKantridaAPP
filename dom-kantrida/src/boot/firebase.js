aa422cebbcf9b9cadffb001f18f608025172ced2
KitsuneSD <phoenixdipf@gmail.com>
2022-02-22 11:20:15 +0100

    Prva verzija kreiranja klijenta i ugovora

--- dom-kantrida/src/boot/firebase.js
+++ dom-kantrida/src/boot/firebase.js
@@ -1,18 +1,28 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import {
+  initializeApp
+} from "firebase/app";
+import {
+  getFirestore
+} from "firebase/firestore";
+import {
+  getAuth
+} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.SENDER_ID,
-  appId: process.env.APP_ID,
+  apiKey: "AIzaSyBG7H711L0O5DPVz-TqRCGC-_b96W2kWec",
+  authDomain: "dom-kantrida.firebaseapp.com",
+  projectId: "dom-kantrida",
+  storageBucket: "dom-kantrida.appspot.com",
+  messagingSenderId: "89658383943",
+  appId: "1:89658383943:web:0d24c894e0680c8e2b9b62"
 };
 
 let firebaseApp = initializeApp(firebaseConfig);
 let db = getFirestore(firebaseApp);
 let auth = getAuth();
 
-export { firebaseApp, db, auth };
+export {
+  firebaseApp,
+  db,
+  auth
+};

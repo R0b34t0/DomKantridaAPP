4745fb6090944892fae8589d5748e04cb7824964
KitsuneSD <phoenixdipf@gmail.com>
2022-02-27 20:58:03 +0100

    Unos klijenata, validacija

--- dom-kantrida/src/components/UnosKlijenta.vue
+++ dom-kantrida/src/components/UnosKlijenta.vue
@@ -1,82 +1,113 @@
 <template>
   <div>
     <q-btn color="primary" label="Dodaj klijenta" @click="handleClick" />
-    <q-dialog v-model="prompt" persistent wid>
+    <q-dialog v-model="state.prompt" persistent wid>
       <q-card style="min-width: 500px">
-        <q-card-section class="q-pt-none">
-          <h6>Dodaj klijenta</h6>
-          <div class="q-ma-lg">
-            <q-input
-              outlined
-              v-model="ime"
-              label="Molimo unesite ime"
-              :dense="dense"
-              :rules="[(val) => !!val || 'Molimo unesite vrijednost']"
-            />
-            <q-input
-              outlined
-              v-model="prezime"
-              label="Molimo unesite prezime"
-              :dense="dense"
-              lazy-rules
-              :rules="[
-                (val) => provjeriVrijednost(val) || 'Molimo unesite vrijednost',
-              ]"
-            />
-            <q-input
-              outlined
-              v-model="email"
-              label="Molimo unesite email"
-              :dense="dense"
-            />
-            <q-input
-              outlined
-              v-model="OIB"
-              label="Molimo unesite OIB"
-              :dense="dense"
-            />
-            <q-input
-              outlined
-              v-model="adresa"
-              label="Molimo unesite adresu"
-              :dense="dense"
-            />
-            <q-select
-              class="q-gutter-md"
-              v-model="odabraniDio"
-              :options="dioGrada"
-              label="Unesite dio grada u kojemu se korisnik nalazi"
-              :dense="dense"
-              outlined
-            />
-            <q-input
-              v-model="datumRodjenja"
-              outlined
-              type="date"
-              hint="Molimo unesite datum rođenja"
-              :dense="dense"
-              :rules="provjeriDatumRule"
-            />
+        <q-form style="width: 100%" @submit="dodajKlijenta(v$)">
+          <q-card-section class="q-pt-none">
+            <h6>Dodaj klijenta</h6>
+            <div class="q-ma-lg">
+              <q-input
+                class="input-field"
+                outlined
+                label="Unesite ime"
+                :dense="dense"
+                v-model="state.ime"
+                :error="v$.ime.$dirty && state.ime"
+              />
+              <div class="error" v-if="v$.ime.$error">
+                {{ v$.ime.$errors[0].$message }}
+              </div>
+              <q-input
+                class="input-field"
+                outlined
+                v-model="state.prezime"
+                label="Molimo unesite prezime"
+                :error="v$.prezime.$dirty && state.prezime"
+                :dense="dense"
+              />
+              <div class="error" v-if="v$.prezime.$error">
+                {{ v$.prezime.$errors[0].$message }}
+              </div>
+              <q-input
+                outlined
+                v-model="state.email"
+                label="Molimo unesite email"
+                :error="v$.email.$dirty && state.email"
+                :dense="dense"
+              />
+              <div class="error" v-if="v$.email.$error">
+                {{ v$.email.$errors[0].$message }}
+              </div>
+              <q-input
+                outlined
+                v-model="state.OIB"
+                :error="v$.OIB.$dirty && state.OIB"
+                label="Molimo unesite OIB"
+                :dense="dense"
+                error-message="OIB mora biti broj sa 11 znakova"
+              />
+              <div class="error" v-if="v$.OIB.$error">
+                {{ v$.OIB.$errors[0].$message }}
+              </div>
 
-            <q-input
-              outlined
-              v-model="brojTelefona"
-              label="Molimo unesite broj telefona"
-              :dense="dense"
+              <q-input
+                outlined
+                v-model="state.adresa"
+                :error="v$.adresa.$dirty && state.adresa"
+                label="Molimo unesite adresu"
+                :dense="dense"
+              />
+              <div class="error" v-if="v$.adresa.$error">
+                {{ v$.adresa.$errors[0].$message }}
+              </div>
+              <q-select
+                class="q-gutter-md"
+                v-model="state.odabraniDio"
+                :options="dioGrada"
+                label="Unesite dio grada u kojemu se korisnik nalazi"
+                :dense="dense"
+                :error="v$.odabraniDio.$dirty && state.odabraniDio"
+                outlined
+              />
+              <div class="error" v-if="v$.odabraniDio.$error">
+                {{ v$.odabraniDio.$errors[0].$message }}
+              </div>
+              <q-input
+                v-model="state.datumRodjenja"
+                outlined
+                type="date"
+                hint="Molimo unesite datum rođenja"
+                :error="v$.datumRodjenja.$dirty && state.datumRodjenja"
+                :dense="dense"
+                :rules="provjeriDatumRule"
+              />
+              <div class="error" v-if="v$.datumRodjenja.$error">
+                {{ v$.datumRodjenja.$errors[0].$message }}
+              </div>
+              <q-input
+                outlined
+                v-model="state.brojTelefona"
+                :error="v$.brojTelefona.$dirty && state.brojTelefona"
+                label="Molimo unesite broj telefona"
+                :dense="dense"
+              />
+              <div class="error" v-if="v$.brojTelefona.$error">
+                {{ v$.brojTelefona.$errors[0].$message }}
+              </div>
+            </div>
+          </q-card-section>
+          <q-card-actions align="right" class="text-primary">
+            <q-btn flat label="Odustani" v-close-popup />
+            <q-btn
+              color="primary"
+              flat
+              label="Dodaj klijenta"
+              type="submit"
+              :disable="!provjeriDatum()"
             />
-          </div>
-        </q-card-section>
-        <q-card-actions align="right" class="text-primary">
-          <q-btn flat label="Odustani" v-close-popup />
-          <q-btn
-            color="primary"
-            flat
-            label="Dodaj klijenta"
-            v-close-popup
-            @click="dodajKlijenta()"
-            :disable="!provjeriDatum()"
-          />
-        </q-card-actions>
+          </q-card-actions>
+        </q-form>
       </q-card>
     </q-dialog>
     <q-btn label="Ugovori" @click="this.handleUgovor = !this.handleUgovor" />
@@ -88,29 +119,31 @@
   </div>
 </template>
 <script>
-import { collection, addDoc } from "firebase/firestore";
+import { ref, computed, reactive, defineComponent } from "vue";
+import Ugovori from "../components/Ugovori.vue";
 
+import { collection, addDoc } from "firebase/firestore";
 import { db } from "src/boot/firebase";
 
-import { defineComponent } from "vue";
-import Ugovori from "../components/Ugovori.vue";
+import useVuelidate from "@vuelidate/core";
+import {
+  required,
+  email,
+  minLength,
+  maxLength,
+  numeric,
+  helpers,
+} from "@vuelidate/validators";
+// import { phoneRegex } from "../utils/regex";
 
 export default defineComponent({
   components: { Ugovori },
   name: "UnosDostave",
+
   data() {
     return {
-      prompt: false,
       handleUgovor: false,
-      ime: "",
-      prezime: "",
-      email: "",
-      OIB: "",
-      adresa: "",
-      dioGrada: ["istok", "zapad"],
-      odabraniDio: "",
-      datumRodjenja: "",
-      brojTelefona: "",
+
       provjeriDatumRule: [
         (vrijednost) =>
           this.provjeriDatum() ||
@@ -118,30 +151,116 @@ export default defineComponent({
       ],
     };
   },
+  setup() {
+    const dioGrada = ["istok", "zapad"];
+
+    const state = reactive({
+      prompt: false,
+      ime: "",
+      prezime: "",
+      email: "",
+      OIB: "",
+      adresa: "",
+      odabraniDio: "",
+      datumRodjenja: "",
+      brojTelefona: "",
+    });
+    const rules = {
+      ime: { required: helpers.withMessage("Ime je obavezno polje", required) },
+      prezime: {
+        required: helpers.withMessage("Prezime je obavezno polje", required),
+      },
+      OIB: {
+        required: helpers.withMessage("OIB je obavezno polje", required),
+        numeric: helpers.withMessage(
+          "OIB mora biti numerička vrijednost",
+          numeric
+        ),
+        minLength: helpers.withMessage(
+          "OIB mora sadržavati 11 znakova",
+          minLength(11)
+        ),
+        maxLength: helpers.withMessage(
+          "OIB mora sadržavati 11 znakova",
+          maxLength(11)
+        ),
+      },
+      email: {
+        required: helpers.withMessage("Email je obavezno polje", required),
+        email: helpers.withMessage(
+          "Email mora biti u obliku primjer@email.com",
+          email
+        ),
+      },
+      datumRodjenja: {
+        required: helpers.withMessage(
+          "Datum rođenja je obavezno polje",
+          required
+        ),
+      },
+      adresa: {
+        required: helpers.withMessage("Adresa je obavezno polje", required),
+      },
+      brojTelefona: {
+        required: helpers.withMessage(
+          "Broj telefona je obavezno polje",
+          required
+        ),
+        numeric: helpers.withMessage(
+          "Broj telefona mora biti numerička vrijednost",
+          numeric
+        ),
+      },
+      odabraniDio: {
+        required: helpers.withMessage(
+          "Morate odabrati korisničku rolu",
+          required
+        ),
+      },
+    };
+    const v$ = useVuelidate(rules, state);
+
+    return {
+      state,
+      v$,
+      dioGrada,
+    };
+  },
   methods: {
-    handleClick() {
-      this.prompt = true;
+    pretvoriDatum(stariDatum) {
+      let myDate = stariDatum;
+      myDate = myDate.split("-");
+      console.log(myDate[0], myDate[1] - 1, myDate[2]);
+      return new Date(myDate[0], myDate[1] - 1, myDate[2]);
     },
-    async dodajKlijenta() {
+    handleClick() {
+      this.state.prompt = true;
+    },
+    async dodajKlijenta(v$) {
+      const formIsValid = await v$.$validate();
+
       // provjeriDatum();
-      const docRef = await addDoc(collection(db, "Klijenti"), {
-        ime: this.ime,
-        prezime: this.prezime,
-        email: this.email,
-        OIB: this.OIB,
-        adresa: this.adresa,
-        odabraniDio: this.odabraniDio,
-        datumRodjenja: this.datumRodjenja,
-        brojTelefona: this.brojTelefona,
-      });
-      console.log("Document written with ID: ", docRef.id);
-      this.ime = "";
-      this.prezime = "";
-      this.email = "";
-      this.OIB = "";
-      (this.adresa = ""), (this.odabraniDio = "");
-      this.datumRodjenja = "";
-      this.brojTelefona = "";
+      if (formIsValid) {
+        const docRef = await addDoc(collection(db, "Klijenti"), {
+          ime: this.state.ime,
+          prezime: this.state.prezime,
+          email: this.state.email,
+          OIB: this.state.OIB,
+          adresa: this.state.adresa,
+          odabraniDio: this.state.odabraniDio,
+          datumRodjenja: this.pretvoriDatum(this.state.datumRodjenja),
+          brojTelefona: this.state.brojTelefona,
+        });
+        console.log("Document written with ID: ", docRef.id);
+        this.ime = "";
+        this.state.prompt = false;
+        this.state.prezime = "";
+        this.state.email = "";
+        this.state.OIB = "";
+        (this.state.adresa = ""), (this.state.odabraniDio = "");
+        this.state.datumRodjenja = "";
+        this.state.brojTelefona = "";
+      }
     },
     provjeriDatum() {
       // uzimanje trenutnog datuma i pretvaranje u istu vrijednost koju dobivamo iz inputa
@@ -153,12 +272,21 @@ export default defineComponent({
         "-" +
         ("0" + date.getDate()).slice(-2);
 
-      return this.datumRodjenja < trenutniDatum;
+      return this.state.datumRodjenja < trenutniDatum;
     },
     provjeriVrijednost(val) {
       console.log(val);
     },
   },
+
   mounted() {},
 });
 </script>
+
+<style >
+.error {
+  color: #c10015;
+  font-size: 12px;
+  margin: -10px 0px 10px 10px;
+}
+</style>
--- dom-kantrida/src/pages/Klijenti.vue
+++ dom-kantrida/src/pages/Klijenti.vue
@@ -1,29 +1,131 @@
 <template>
   <div class="q-pa-md">
     <UnosKlijenta class="flex col-1" />
+
+    <q-table
+      :rows="klijenti"
+      :columns="columns"
+      row-key="id"
+      no-data-label="U bazi ne postoji popis korisnika"
+    >
+      <!-- :loading="loading" -->
+    </q-table>
   </div>
 </template>
 <script>
 import { defineComponent } from "vue";
 import "../css/app.css";
 import { db } from "src/boot/firebase";
-import { collection, query, getDocs, where } from "firebase/firestore";
+import {
+  collection,
+  query,
+  getDocs,
+  where,
+  onSnapshot,
+} from "firebase/firestore";
 import UnosKlijenta from "../components/UnosKlijenta.vue";
 import { formatDate, formatDateDisplay } from "../utils/formatDate";
 
-const today = new Date();
+const columns = [
+  {
+    name: "id",
+    required: true,
+    label: "ID",
+    align: "left",
+    field: "id",
+    sortable: false,
+  },
+  {
+    name: "ime",
+    align: "left",
+    label: "Ime",
+    field: "ime",
+    sortable: true,
+  },
+  {
+    name: "prezime",
+    align: "left",
+    label: "Prezime",
+    field: "prezime",
+    sortable: true,
+  },
+  {
+    name: "OIB",
+    required: true,
+    label: "OIB",
+    align: "left",
+    field: "OIB",
+    sortable: true,
+  },
+  {
+    name: "adresa",
+    align: "center",
+    label: "Adresa",
+    field: "adresa",
+    sortable: true,
+  },
+  {
+    name: "brojTelefona",
+    align: "left",
+    label: "Broj telefona",
+    field: "brojTelefona",
+    sortable: false,
+  },
+  {
+    name: "datumRodjenja",
+    align: "left",
+    label: "Datum rođenja",
+    field: "datum Rodjenja",
+    sortable: true,
+  },
+  {
+    name: "email",
+    align: "left",
+    label: "E-mail",
+    field: "email",
+    sortable: true,
+  },
+  {
+    name: "odabraniDio",
+    align: "left",
+    label: "Dio grada",
+    field: "odabraniDio",
+    sortable: true,
+  },
+];
+
 export default defineComponent({
   components: { UnosKlijenta },
   name: "Dostave",
   data() {
     return {
       klijenti: [],
+      loading: true,
     };
   },
   methods: {},
   mounted() {
-    // this.getData(this.izabraniDatum);
-    // tu vuci podatke iz baze kada pokreces page
+    // this.klijenti.splice(0, this.klijenti.length);
+    const q = query(collection(db, "Klijenti"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      this.klijenti.length = 0;
+
+      querySnapshot.forEach((doc) => {
+        this.klijenti.push({
+          id: doc.id,
+          ime: doc.data().ime,
+          prezime: doc.data().prezime,
+          adresa: doc.data().adresa,
+          odabraniDio: doc.data().odabraniDio,
+          brojTelefona: doc.data().brojTelefona,
+          datumRodjenja: new Date(
+            doc.data().datumRodjenja.seconds * 1000
+          ).toLocaleDateString("en-GB"),
+          email: doc.data().email,
+        });
+        // console.log(doc.data());
+      });
+    });
   },
 });
 </script>
